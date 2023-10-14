93c235886f63973d8d78542db555dfdc84d33809
Richard Funari <sharplead@hotmail.com>
2023-10-14 00:39:53 -0300

    adicionado um try

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -14,30 +14,30 @@ public class ContaTerminal {
 
 
         //criação do ojbeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua agência");
+            String agencia = scanner.next();
+
+            System.out.println("Digite o número da conta");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("Saldo disponível para saque é de:");
+            double saldoConta = scanner.nextDouble();
 
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
-
-        System.out.println("Digite sua agência");
-        String agencia = scanner.next();
-
-        System.out.println("Digite o número da conta");
-        int numeroConta = scanner.nextInt();
-
-        System.out.println("Saldo disponível para saque é de:");
-        double saldoConta = scanner.nextDouble();
-
-
-        //imprimir os dados obtidos pelo usuário
-        System.out.println("Olá " + nome + " " + sobrenome + ",");
-        System.out.println("obrigado por criar uma conta em nosso banco. Sua agência é: " + agencia + ",");
-        System.out.println("sua conta é: " + numeroConta);
-        System.out.println("e o seu saldo de " + saldoConta + " já está disponível para saque.");
+            //imprimir os dados obtidos pelo usuário
+            System.out.println("Olá " + nome + " " + sobrenome + ",");
+            System.out.println("obrigado por criar uma conta em nosso banco. Sua agência é: " + agencia + ",");
+            System.out.println("sua conta é: " + numeroConta);
+            System.out.println("e o seu saldo de " + saldoConta + " já está disponível para saque.");
+        }
 
 
     }
